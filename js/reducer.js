1f94bdc9f0af2506eb1690ff466b8d2be948fd20
McCray <tubach75@hotmail.com>
2021-09-03 11:11:44 -0400

    commit tree d6fa1022504ecd5a65e7c262b036158c2fe58d2e

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,27 @@
-// add code snippets from README
+let state;
+function reducer(state = {count: 2300}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1900}
+    default:
+      return state;
+  }
+}
+
+function dispatch(action){
+  state = reducer(state, action);
+  render();
+}
+
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+
+dispatch({type: '@@INTI'})
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
